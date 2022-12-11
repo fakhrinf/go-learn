faee57e24f87079aa0b7c39e2a9e5aa7a76fffd1
Fakhri Nurfauzan <fakhrinf@hotmail.com>
2022-12-11 13:22:04 +0000

    change tab into spaces

--- greetings.go
+++ greetings.go
@@ -5,7 +5,7 @@ import "fmt"
 type greetings []string
 
 func (g greetings) cetak() {
-	for _, greet := range g {
-		fmt.Println(greet)
-	}
+    for _, greet := range g {
+        fmt.Println(greet)
+    }
 }
--- main.go
+++ main.go
@@ -7,53 +7,53 @@ import "fmt"
 
 func main() {
 
-	// normal variable declaration
-	// var message string = "hello go in gitpod!"
+    // normal variable declaration
+    // var message string = "hello go in gitpod!"
 
-	// abv variable declaration
-	message := "hello go in gitpod"
+    // abv variable declaration
+    message := "hello go in gitpod"
 
-	// replace variable value
-	message = "second time hello go in gitpod"
+    // replace variable value
+    message = "second time hello go in gitpod"
 
-	// replace variable value by function
-	message = hello()
+    // replace variable value by function
+    message = hello()
 
-	// print message using parameter
-	sayWhat("say something using parameter")
+    // print message using parameter
+    sayWhat("say something using parameter")
 
-	// print message into console
-	fmt.Println(message)
+    // print message into console
+    fmt.Println(message)
 
-	// slices (array without specify the length)
-	_greetings := []string{"Hello", "Hi", "ola", "sup!"}
+    // slices (array without specify the length)
+    _greetings := []string{"Hello", "Hi", "ola", "sup!"}
 
-	// for loop
-	for i := 0; i < len(_greetings); i++ {
-		fmt.Println(_greetings[i])
-	}
+    // for loop
+    for i := 0; i < len(_greetings); i++ {
+        fmt.Println(_greetings[i])
+    }
 
-	// add data into slices
-	_greetings = append(_greetings, "OI")
+    // add data into slices
+    _greetings = append(_greetings, "OI")
 
-	// for loop with range
-	for _, greet := range _greetings {
-		fmt.Println(greet)
-	}
+    // for loop with range
+    for _, greet := range _greetings {
+        fmt.Println(greet)
+    }
 
-	// create greetings from another file with custom type
-	greetAgain := greetings{"Whaaaaats up brooo!"}
+    // create greetings from another file with custom type
+    greetAgain := greetings{"Whaaaaats up brooo!"}
 
-	// call function to print
-	greetAgain.cetak()
+    // call function to print
+    greetAgain.cetak()
 }
 
 // function declaration with return type
 func hello() string {
-	return "Hello go from function!"
+    return "Hello go from function!"
 }
 
 // functio with parameter
 func sayWhat(message string) {
-	fmt.Println(message)
+    fmt.Println(message)
 }
